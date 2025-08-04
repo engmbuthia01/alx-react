2190f82a8438995855498eb877249245fe99cee1
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-04 02:42:07 -0400

    created a notification function

--- 0x01-react_intro/task_1/src/Notifications.js
+++ 0x01-react_intro/task_1/src/Notifications.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Notifications.css';
 
-export function Notifications() {
+export default function Notifications() {
     return (
         <div className='Notifications'>
             <p>Here is the list of notifications</p>
--- 0x01-react_intro/task_1/src/utils.js
+++ 0x01-react_intro/task_1/src/utils.js
@@ -2,7 +2,7 @@ export function getFullYear() {
     return new Date().getFullYear();
 }
 
-export default getFooterCopy(isIndex) {
+export function getFooterCopy(isIndex) {
     if (isIndex) {
         return "ALX";
     } else {
