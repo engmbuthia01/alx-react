743bd8e382455d9b1570f8f6db958573e1c73fd8
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-06 15:52:09 -0400

    wrote tests for the three functions in utils.js

--- 0x01-react_intro/task_0/src/App.js
+++ 0x01-react_intro/task_0/src/App.js
@@ -9,11 +9,9 @@ function App() {
         <img src={alxlogo} alt='ALX Logo' />
         <h1>School dashboard</h1>
       </div >
-
       <div className='App-body'>
         <p>Login to access the full dashboard</p>
       </div>
-
       <div className='App-footer'>
         <p>Copyright 2020 - ALX</p>
       </div>
