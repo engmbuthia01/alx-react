f531a2657f85bf78ea6d3a721c47bdd522146a3a
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-07 06:38:02 -0400

    downgraded react version to support enzyme and configured it in the setupTests.js file

--- 0x01-react_intro/task_3/src/setupTests.js
+++ 0x01-react_intro/task_3/src/setupTests.js
@@ -1,5 +1,5 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// setupTests.js or wherever you set up Enzyme
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
