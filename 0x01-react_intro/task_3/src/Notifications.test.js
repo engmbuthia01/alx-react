3f32bf57f19ddfc778557dc748cc61f5a2de5d67
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-08 03:09:40 -0400

    created tests for the notifications component

--- 0x01-react_intro/task_3/src/Notifications.test.js
+++ 0x01-react_intro/task_3/src/Notifications.test.js
@@ -5,15 +5,21 @@ import { shallow } from 'enzyme';
 describe('Notifications compnent tests', () => {
     it('renders Notifications without failing', () => {
         const component = shallow(<Notifications />);
+        expect(component).toBeDefined();
+    });
+
+    it("renders ul", () => {
+        const notification = shallow(<Notifications />);
+        expect(notification.find("ul")).toBeDefined();
     });
 
     it('renders three list items', () => {
         const component = shallow(<Notifications />);
-        expect(component.find('ul')).toBeDefined();
+        expect(component.find('li')).toHaveLength(3);
     });
 
     it('renders the text Here is the list of notifications', () => {
         const component = shallow(<Notifications />);
-        expect(component.find('p')).toBeDefined();
+        expect(component.find('p').prop('children')).toBe('Here is the list of notifications');
     });
 });
