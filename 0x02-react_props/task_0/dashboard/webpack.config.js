4874962334c453f9b2ed90c33f7f6548d90aa585
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-14 00:26:04 -0400

    copied the contents of the previous task recurcively

---
+++ 0x02-react_props/task_0/dashboard/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // starting point
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    devtool: 'inline-source-map', // for debugging
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        hot: true, // hot reloading
+        open: true,
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,  // Support both .js and .jsx
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'], // CSS support
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // keep things simple
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
