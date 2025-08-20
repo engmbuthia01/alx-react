c4ed6a36edc72c18d6018ff931815f373e5b824f
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-19 23:53:07 -0400

    added a header.js

---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import './header.css';
+
+$('#root').append(`
+  <header id="header">
+    <div id="logo"></div>
+    <h1>ALX Dashboard</h1>
+  </header>
+`);
+
+console.log('Init header');
