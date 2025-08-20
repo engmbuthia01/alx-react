2f2597d075b4281929083e8942e8c9e1d7504150
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-20 01:35:24 -0400

    added a dev server in the web.config file

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -3,7 +3,13 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path = path.resolve(__dirname, 'dist'),
-
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        hot: 'true',
+        open: 'true',
+        port: 3000
     },
 }
