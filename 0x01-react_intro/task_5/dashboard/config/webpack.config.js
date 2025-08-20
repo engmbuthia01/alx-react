916dbe0b72d9bf37b0ff0eacb1f0c7ab30374890
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-20 02:55:54 -0400

    added loaders

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -6,10 +6,35 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    mode: 'development',
+    devtool: 'inline-source-map',
     devServer: {
         static: path.resolve(__dirname, 'dist'),
         hot: 'true',
         open: 'true',
         port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                        },
+                    },
+                ],
+            },
+        ],
     },
 }
