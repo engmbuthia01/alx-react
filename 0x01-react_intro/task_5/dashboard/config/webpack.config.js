559bf4747031b5e4750174a75e9582e11d4e6891
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-20 03:48:43 -0400

    added babel presets

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -37,6 +37,15 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.(js|jsx)$/i,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
+            },
         ],
     },
 };
