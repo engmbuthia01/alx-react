f5aa818e3247dbae6679a6ab34afbbb814afdcdb
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-21 03:50:27 -0400

    installed jest and enzyme

--- 0x01-react_intro/task_5/dashboard/src/App/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -14,9 +14,9 @@ function App() {
                 <p>Login to access the full dashboard</p>
                 <form>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" name="email"></input>
+                    <input type="email" id="email" name="email"></input>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" name="password"></input>
+                    <input type="password" id="password" name="password"></input>
                     <button>OK</button>
                 </form>
             </div>
