d7c58d543b9e7dd119a29d7a656cc5594af142e5
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-21 02:45:11 -0400

    added htmlwebpack plugin

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './dist/index.html',
+            template: './public/index.html',
         }),
     ],
     module: {
--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,14 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App/App";
-import Notifications from "./Notifications/Notifications";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App/App';
+import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications/Notifications';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <div id="root-notifications">
-      <Notifications />
-    </div>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+const rootNotifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
