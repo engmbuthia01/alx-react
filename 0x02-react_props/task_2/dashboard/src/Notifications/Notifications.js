43e53847d5b10bb8dad21108d14010fd33cfb340
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-24 23:13:42 -0400

    passed the props to the NotificationItem component

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -17,9 +17,12 @@ function Notifications() {
             </button>
             <p>Here is the list of notifications</p>
             <ul>
-                <NotificationItem data="default" />
-                <NotificationItem data="urgent" />
-                <NotificationItem data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
+                <NotificationItem type="default" value="New course available" />
+                <NotificationItem type="urgent" value="New resume available" />
+                <NotificationItem
+                    type="urgent"
+                    html={{ __html: "<strong>Urgent requirement</strong> - complete by EOD" }}
+                />
             </ul>
         </div>
     );
