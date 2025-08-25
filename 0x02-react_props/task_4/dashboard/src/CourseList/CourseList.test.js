8ac4a89482a86e8e33928d954fd602d0ba178602
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-08-25 16:54:22 -0400

    created a new component named CourseList

---
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseList.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { shallow } from "enzyme";
+import CourseList from "./CourseList";
+import CourseListRow from "./CourseListRow";
+
+describe("CourseList tests", () => {
+    it("renders without crashing", () => {
+        shallow(<CourseList />);
+    });
+
+    it("renders 5 rows", () => {
+        const wrapper = shallow(<CourseList />);
+        expect(wrapper.find(CourseListRow)).toHaveLength(5);
+    });
+});
