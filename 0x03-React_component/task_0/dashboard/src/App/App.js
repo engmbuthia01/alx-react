c445215a6ca7b64d3b5dd36e5d6ccf6a78d53ea8
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-09-22 01:17:23 -0400

    created a class component out of a simple component

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Login from "../Login/Login";
@@ -20,19 +20,21 @@ const listNotifications = [
     { id: 3, type: "urgent", html: getLatestNotification() },
 ];
 
-function App(isLoggedIn) {
-    return (
-        <React.Fragment>
-            <div className="App">
-                <div className="heading-section">
-                    <Notifications listNotifications={listNotifications} />
-                    <Header />
+class App extends Component(isLoggedIn) {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="App">
+                    <div className="heading-section">
+                        <Notifications listNotifications={listNotifications} />
+                        <Header />
+                    </div>
+                    {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+                    <Footer />
                 </div>
-                {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-                <Footer />
-            </div>
-        </React.Fragment>
-    );
+            </React.Fragment >
+        );
+    }
 }
 
 App.defaultProps = {
