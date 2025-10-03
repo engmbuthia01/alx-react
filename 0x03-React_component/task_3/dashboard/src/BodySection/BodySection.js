404eb24e3bf8ec0cb1048236ed8a19455b6ca8cd
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-10-03 13:53:23 -0400

    used the spread operator to reuse props

--- 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.js
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -5,13 +5,10 @@ class BodySection extends Component {
     render() {
         const { title, children } = this.props;
         return (
-            <React.Fragment>
-                <div className="bodySection">
-                    <h2>{title}</h2>
-                    {children}
-                </div>
-
-            </React.Fragment>
+            <div className="bodySection">
+                <h2>{title}</h2>
+                {children}
+            </div>
         );
     }
 }
