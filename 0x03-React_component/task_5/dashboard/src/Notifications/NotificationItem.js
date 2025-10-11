9919af2edd0e2deaa0e2dff6d766bd145edd5db7
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-10-11 04:49:15 -0400

    declared a pure component

---
+++ 0x03-React_component/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./Notifications.css";
+import PropTypes from "prop-types";
+
+class NotificationItem extends Component {
+    render() {
+        const { type, value, html, markAsRead } = this.props;
+        return (
+            <>
+                {type && value ? <li onClick={() => markAsRead(id)} data-notification-type={type}>{value}</li> : null}
+                {html ? <li onClick={() => markAsRead(id)} data-urgent dangerouslySetInnerHTML={{ __html: html }}></li> : null}
+            </>
+        );
+    }
+
+}
+
+NotificationItem.propTypes = {
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    html: PropTypes.shape({
+        __html: PropTypes.string,
+    }),
+    markAsRead: PropTypes.func,
+};
+
+NotificationItem.defaultProps = {
+    type: "default",
+    value: "",
+    html: null,
+    markAsRead: () => { },
+};
+
+export default NotificationItem;
