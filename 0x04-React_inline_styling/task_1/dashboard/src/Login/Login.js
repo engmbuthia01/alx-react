143417e1a20846d316a30814d6aaa7250773965a
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-10-15 01:47:27 -0400

    added inline styling to the Login component and deleted the Login.css file

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,21 +1,33 @@
 import React from "react";
-import './Login.css';
+import { StyleSheet, css } from "aphrodite";
 
 function Login() {
     return (
         <React.Fragment>
-            <div className="App-body">
+            <div className={css(styles.AppBody)}>
                 <p>Login to access the full dashboard</p>
                 <form>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email"></input>
+                    <input type="email" id="email" name="email" className={css(styles.input)}></input>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password"></input>
+                    <input type="password" id="password" name="password" className={css(styles.input)}></input>
                     <button>OK</button>
                 </form>
             </div >
         </React.Fragment>
     );
 }
+
+const styles = StyleSheet.create({
+    AppBody: {
+        fontSize: "1rem",
+        padding: "2em",
+        borderBottom: "3px solid #e0354b",
+        height: "75%",
+    },
+    input: {
+        margin: "10px",
+    },
+});
 
 export default Login;
