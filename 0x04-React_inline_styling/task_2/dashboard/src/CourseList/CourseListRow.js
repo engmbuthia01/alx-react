e8c773f1e5f139b07dfb52442a265a291aecb585
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-10-16 08:12:17 -0400

    added inline styling to the CourseList Component

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,17 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
-    const headerStyle = { backgroundColor: "#deb5b545" };
-    const rowStyle = { backgroundColor: "#f5f5f5ab" };
     return (
-        <tr style={rowStyle}>
+        <tr className={isHeader ? css(styles.header) : css(styles.normal)}>
             {isHeader ? (
                 textSecondCell === null ? (
-                    <th style={headerStyle} colSpan={2}>{textFirstCell}</th>
+                    <th colSpan={2}>{textFirstCell}</th>
                 ) : (
                     <>
-                        <th style={headerStyle}>{textFirstCell}</th>
+                        <th>{textFirstCell}</th>
                         <th style={headerStyle}>{textSecondCell}</th>
                     </>
                 )
@@ -25,18 +24,25 @@ function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
     );
 }
 
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: "#deb5b545",
+    },
 
-CourseListRow.PropTypes = {
+    normal: {
+        backgroundColor: "#f5f5f5ab",
+    },
+});
+
+CourseListRow.propTypes = {
     isHeader: PropTypes.bool,
     textFirstCell: PropTypes.string.isRequired,
-    textSecondCell: PropTypes.oneOfType(PropTypes.string, PropTypes.number),
+    textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
-
 
 CourseListRow.defaultProps = {
     isHeader: false,
     textSecondCell: null,
 };
-
 
 export default CourseListRow;
